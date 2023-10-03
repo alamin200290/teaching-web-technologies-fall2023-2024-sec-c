e9e64d75d285f6561b87a4865e3b8dc8d189df4b
alamin <amin.se.aiub@gmail.com>
2023-10-03 15:49:26 +0600

    into php

---
+++ php/test.php
@@ -0,0 +1,49 @@
+<?php 
+    // $name = 'alamin';
+    // $id = 123;
+    // $cgpa = 3.00;
+    // $student = [1, 'alamin', 3.5];
+    // $student[0];
+
+    // $students =[ 
+    //         [1, 'alamin', 3.3],
+    //         [2, 'xyz', 3.5],
+    //         [3, 'abc', 3.2]
+    // ];
+    // $student[2][2];
+
+    // $student =  ['id'=>1, 'name'=>'alamin', 'cgpa'=>3.3];
+    // $student['id'];
+
+    $students = [
+            's1'=>['id'=>1, 'name'=>'alamin', 'cgpa'=>3.3],
+            's2'=>['id'=>2, 'name'=>'xyz', 'cgpa'=>3.34],
+            's3'=>['id'=>3, 'name'=>'abc', 'cgpa'=>3.5]
+        ];
+
+//    $students['s3']['cgpa'];
+
+    // if(){
+
+    // }else if(){
+    // }
+
+    // function abc($a=0, $b=5){
+    //     return $a+$b;
+    // }
+    
+    // $total = abc(1);
+    // for($i=0; $i<10; $i++){
+    // }
+
+    //print('test');
+    //echo "test";
+    //print_r($students);
+
+    for($i=0; $i<10; $i++){
+        echo "<h1>This is webtech class</h1>";
+    }  
+
+?>
+
+<h1>TEST</h1>
